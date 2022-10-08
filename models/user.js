659ae651c57192ea17f12db0350b94556e09ef7c
JaimesAntonio <jaimes.antonio@hotmail.com>
2022-10-07 21:05:08 -0500

    first commit conection to mongoDB

---
+++ models/user.js
@@ -0,0 +1,14 @@
+
+// se crea modelo del objeto a guardar
+const mongoose = require('mongoose')
+ 
+let UserSchema = new mongoose.Schema({
+    id: Number,
+    nombre: String,
+    apellidos: String,
+    correo: String,
+    telefono: String,
+    password: String
+})
+ 
+module.exports = mongoose.model('usuario', UserSchema, 'usuario')
